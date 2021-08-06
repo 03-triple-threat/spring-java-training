0b545b8e05f31b8235aa11daf5aa970bfa3c0363
AJ Hurtado <ajhurtado3@gmail.com>
2021-08-06 16:13:38 -0500

    training

--- src/main/java/com/example/demojavaspring/user/User.java
+++ src/main/java/com/example/demojavaspring/user/User.java
@@ -2,12 +2,17 @@ package com.example.demojavaspring.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
     private Integer id;
 
+    @Size(min=3)
     private String name;
 
+    @Past
     private Date birthDate;
 
     protected User() {
--- src/main/java/com/example/demojavaspring/user/UserResource.java
+++ src/main/java/com/example/demojavaspring/user/UserResource.java
@@ -1,17 +1,23 @@
 package com.example.demojavaspring.user;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
-    
+
     @Autowired
     private UserDaoService service;
 
@@ -22,12 +28,39 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user == null) 
+            throw new UserNotFoundException("id: " + id);
+        
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
+
+        /**
+         * builds a location for where the newly created resource lives
+         */
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(savedUser.getId()).toUri();
+
+        /**
+         * returns the Response code for created as well as the URI location built
+         */
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteUserById(id);
+
+        if(user == null) 
+            throw new UserNotFoundException("id: " + id);
+        
     }
 
 }
